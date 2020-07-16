1bc51a481a4a6607f2492b7253a4a6bf69b5fe8e
shirleyatgithub <56689341+shirleyatgithub@users.noreply.github.com>
2020-07-16 11:14:19 +0200

    organize the code

--- consumerudp.py
+++ consumerudp.py
@@ -1,61 +1,7 @@
 """DAVIS346 test example.
 
-Author: Yuhuang Hu
-Email : duguyue100@gmail.com
-这个版本是把predict作为主进程，但是producer在产生了4次数据后，就get不到数据了。
-Device ID: 1
-Device is master.
-Device Serial Number: 00000002
-Device String: DAVIS ID-1 SN-00000002 [4:12]
-Device USB bus Number: 4
-Device USB device address: 12
-Device size X: 346
-Device size Y: 260
-Logic Version: 18
-Background Activity Filter: True
-Color Filter 0 <class 'int'>
-False
-frame shape 0
-frame shape 0
-get_event
-data not none
-produce time 11.95 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 68.867 ms
-write ard time 0.107 ms
-predition done
-produce time 11.367 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 4.377 ms
-write ard time 0.041 ms
-predition done
-produce time 7.382 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 4.385 ms
-write ard time 0.039 ms
-predition done
-produce time 5.541 ms
-classify recv msg (224, 224, 1)
-index 0 predict time 4.105 ms
-write ard time 0.046 ms
-predition done
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
+Author: Shasha Guo
+Email : shirleyguo19@gmail.com
 """
 from __future__ import print_function
 
@@ -100,7 +46,7 @@ from utils import recv_into
 import numpy as np
 MAXB = 10000000
 
-
+# port of socket
 PORT = 12000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 address = ("", PORT)
@@ -110,42 +56,21 @@ server_socket.bind(address)
 #The following line is for serial over GPIO
 
 
-# savepath = './dvsoutrec2/tmp0/'
-
 class_name = ['joker', 'other']
 
-#人脸识别分类器本地存储路径
-cascade_path = "./jokeravi.xml"   
-
-# modelpath = './184binv32.h5'
-# modelpath = './alexnet184grayv12.h5'
+# the with file with postfix .h5, e.g. afnorm224v1.h5
 modelpath = sys.argv[1]
-
+# the size of frame to be resized to
 resize = int(sys.argv[2])
-
-port = sys.argv[3]
-
-
-ard = serial.Serial(port,9600,timeout=5)
-
-    
-# files = os.listdir(savepath)
-# savedcounter = len(files)
-# print(avipath, modelpath, savedcounter)      
-color = (0, 255, 0)
+# the arduino port
+ardFlag = 0
+if len(sys.argv) > 3:
+    port = sys.argv[3]
+    ard = serial.Serial(port, 9600, timeout=5)
+    ardFlag = 1
 
 model = load_model(modelpath)
 
-    
-
-
-x = 112
-y = 112
-
-w = 50
-h = 50
-    
-#循环检测识别人脸
 count = 0
 
 # savedcounter = 30000
@@ -189,7 +114,7 @@ if __name__ == '__main__':
             stime = datetime.now()
             receive_data = np.fromstring(receive_data, dtype=np.uint8)
             # print(receive_data)
-            # print("接收到了客户端 %s 传来的数据: %s" % (client_address, np.reshape(receive_data,[3,3,1])))
+            # print("client %s sending data: %s" % (client_address, np.reshape(receive_data,[3,3,1])))
             img1 = np.reshape(receive_data, [resize, resize])
             # print('recv from udp', img1.shape)
             # img1 = cv2.imread(savepath + 'newest.jpg', 0)
@@ -207,7 +132,7 @@ if __name__ == '__main__':
             tmp = model.predict(img4[None, :]) 
 
             etime = datetime.now()
-            # print(pred)
+
             pred = list(tmp[0])
                 
             index = pred.index(max(pred))
@@ -215,10 +140,11 @@ if __name__ == '__main__':
             # print('index', index, 'predict time', (etime - stime).microseconds / 1000, 'ms')
             # label = class_name[pred.index(max(pred))]
 
-            stime = datetime.now()
-            setTemp1 = str(index)
-            ard.write(setTemp1.encode())
-            etime = datetime.now()
+            if ardFlag:
+                stime = datetime.now()
+                setTemp1 = str(index)
+                ard.write(setTemp1.encode())
+                etime = datetime.now()
             
             # print('write ard time', (etime - stime).microseconds / 1000, 'ms')
 
@@ -227,11 +153,10 @@ if __name__ == '__main__':
             cv2.imshow("image", img4)
             if index != 0:
                 print('finger index', index)
-
+                # if joker, stop 0.5ms for show
                 time.sleep(0.5)
 
-
-
+                # write the frames into jpg
                 # stime = datetime.now()
                 # jpgname = savepath + str(savedcounter) + '.jpg'
                 # cv2.imwrite(jpgname, img1)
--- producerudp.py
+++ producerudp.py
@@ -1,61 +1,8 @@
-"""DAVIS346 test example.
+"""trixie producer: getting data from davis346, generating frames based on 3-sigma rule,
+and sending the data to consumer.
 
-Author: Yuhuang Hu
-Email : duguyue100@gmail.com
-这个版本是把predict作为主进程，但是producer在产生了4次数据后，就get不到数据了。
-Device ID: 1
-Device is master.
-Device Serial Number: 00000002
-Device String: DAVIS ID-1 SN-00000002 [4:12]
-Device USB bus Number: 4
-Device USB device address: 12
-Device size X: 346
-Device size Y: 260
-Logic Version: 18
-Background Activity Filter: True
-Color Filter 0 <class 'int'>
-False
-frame shape 0
-frame shape 0
-get_event
-data not none
-produce time 11.95 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 68.867 ms
-write ard time 0.107 ms
-predition done
-produce time 11.367 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 4.377 ms
-write ard time 0.041 ms
-predition done
-produce time 7.382 ms
-classify recv msg (224, 224, 1)
-get_event
-data not none
-index 0 predict time 4.385 ms
-write ard time 0.039 ms
-predition done
-produce time 5.541 ms
-classify recv msg (224, 224, 1)
-index 0 predict time 4.105 ms
-write ard time 0.046 ms
-predition done
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
-get_event
+Author: Shasha Guo
+Email : shirleyguo19@gmail.com
 """
 from __future__ import print_function
 
@@ -96,37 +43,15 @@ from multiprocessing import Process
 import socket
 import numpy as np
 from utils import send_from
-#SOCK_DGRAM    udp mode
+#SOCK_DGRAM udp mode
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('', 12000)
 
-
 class_name = ['joker', 'other']
-
-#人脸识别分类器本地存储路径
-cascade_path = "./jokeravi.xml"   
-
-# modelpath = './184binv32.h5'
-# modelpath = './alexnet184grayv12.h5'
-# modelpath = sys.argv[1]
+# the size of frame to be resized to
 resize = int(sys.argv[1])
 
-    
-# files = os.listdir(savepath)
-# savedcounter = len(files)
-# print(avipath, modelpath, savedcounter)      
-color = (0, 255, 0)
-
-# model = load_model(modelpath)
-
-x = 112
-y = 112
-
-w = 50
-h = 50
-    
-#循环检测识别人脸
 count = 0
 
 # savedcounter = 30000
@@ -157,22 +82,17 @@ device.set_bias_from_json("./configs/davis346_config.json")
 clip_value = 3
 histrange = [(0, v) for v in (260, 346)]
 
-
-shapelike = [260,346,1]
 def get_event(device):
     data = device.get_event()
-
     return data
 
-
 num_packet_before_disable = 0
-
 
 rectifyPolarities = True
 
 rangeNormalizeFrame = 255
 
-
+# constant event number to form a frame
 CONSTNUM = 3000
 
 frame = np.zeros([])
@@ -180,16 +100,12 @@ print('frame shape', len(frame.shape))
 
 img2 = np.zeros([])
 
-
-
 def producer():
 
     frame = np.zeros([])
     print('frame shape', len(frame.shape))
     while True:
         try:
-
-            
             i = 0
 
             # print('---------------------------------fetch data')
@@ -205,13 +121,16 @@ def producer():
                  num_imu_event) = data
 
                 if num_pol_event != 0:
-                   
+
+                    # get enough events to form a frame as parameter indicated
 
                     if leng > 0:
+                        # if the packet is larger than the number, discard the extra events
                         if frame.shape[0] > CONSTNUM:
                             frame = frame[:CONSTNUM]
                             # print('--------------------enough data')
                         else:
+                            # if the packet is smaller than the number, store the events and wait for next packet
                             frame = np.vstack([frame, pol_events])
 
                             if frame.shape[0] > CONSTNUM:
@@ -244,42 +163,22 @@ def producer():
 
         
                     imgtmp = img_on - img_off
-                     
-
                     tmpvar = np.reshape(imgtmp, [imgtmp.shape[0] * imgtmp.shape[1],1]).astype('uint8')
-
-
                     pixmap = np.zeros(tmpvar.shape)
 
                     n = len(pixmap)
 
-
                     tmpvar = tmpvar * 1. / np.max(tmpvar)
-
-                    SUM = np.sum(tmpvar)
-                    COUNT = np.sum(tmpvar > 0)
-                    MEAN = SUM / COUNT
-               
-
                     tmpvar2 = ma.masked_values(tmpvar, 0)
-
-                    mean3 = np.mean(tmpvar2.compressed())
-
                     var3 = np.var(tmpvar2.compressed())
 
-
                     sig = math.sqrt(var3)
-
-         
-
                     if sig < (0.1 / 255.0):
                         sig = 0.1 / 255.0
-
 
                     pixmap = np.zeros(tmpvar.shape)
 
                     # print('pixmap', pixmap.shape)
-
 
                     numSDevs = 3.
                     mean_png_gray = 0 if rectifyPolarities == True else (127. / 255)
@@ -291,47 +190,28 @@ def producer():
                     halfRange = 0. if rectifyPolarities == True else (numSDevs * sig)
                     rangenew = 1.
 
-                    nonZeroCount = 0
-
-                    
                     n = len(pixmap)
-
-            
-                    nonZeroCount = np.sum(tmpvar > 0)
-      
 
                     tmpvar[tmpvar > 0.] = ((tmpvar[tmpvar > 0.] + halfRange)*rangenew) / fullrange
                     tmpvar[tmpvar > 1.] = 1.
                     tmpvar[tmpvar == 0.] = mean_png_gray
 
-
-                    
-
                     pixmap = tmpvar * 1.0 * rangeNormalizeFrame
 
                     # print('tmpvar unique after', np.unique(pixmap))
 
-
                     fimg = np.reshape(pixmap, imgtmp.shape)
-
 
                     img1 = fimg.astype('uint8')
 
-                    # print(' unique img2', np.unique(img2))
-
-                    # img2 = img1 * 1. / 255
-
-                    # classify
                     b = datetime.now()
 
-                    img4 = cv2.resize(img1, (resize, resize),interpolation=cv2.INTER_NEAREST)
-                    # imgf = np.reshape(imgf, (resize, resize, 1))
-                    # img4 = np.reshape(imgf, (resize, resize, 1))
+                    img4 = cv2.resize(img1, (resize, resize), interpolation=cv2.INTER_NEAREST)
 
                     print('process time', (b-a).microseconds / 1000, 'ms')
 
                     try:
-                        
+                        # sending data to consumer
                         a = datetime.now()
                         data = img4.tostring()
                         client_socket.sendto(data, server_address)
